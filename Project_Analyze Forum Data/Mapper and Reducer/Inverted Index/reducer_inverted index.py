5ac353befe15617ec261382fc212007aac6bf098
Allen <jfhsieh823@gmail.com>
2017-07-09 10:41:49 -0400

    Update reducer for Inverted Index

--- Project_Analyze Forum Data/Mapper and Reducer/Inverted Index/reducer_inverted index.py
+++ Project_Analyze Forum Data/Mapper and Reducer/Inverted Index/reducer_inverted index.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 
 import sys
+import re
 
 # Loop around the data
 # It will be in the format key\tval
+
+count = 0
+list_ID = []
 
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
@@ -11,3 +15,16 @@ for line in sys.stdin:
         continue
 
     thisID, thisBody = data_mapped
+
+    regex = r'\b(fantastic)\b'
+    if re.search(regex, thisBody, re.IGNORECASE):
+        count += 1
+    regex = r'\b(fantastically)\b'
+    if re.search(regex, thisBody, re.IGNORECASE):
+        list_ID.append[thisID]
+
+print "Count of word 'fantastic'", "\t", count
+
+list_ID.sort()
+
+print "List of ID contained work 'fantastically'", "\t", list_ID
